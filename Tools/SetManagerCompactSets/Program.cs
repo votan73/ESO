3bd350f733ba6c206c4d6d079c29ccb9520dc9e0
votan <votan@ogitix.de>
2016-09-10 10:44:44 +0200

    Qualify sets

--- Tools/SetManagerCompactSets/Program.cs
+++ Tools/SetManagerCompactSets/Program.cs
@@ -31,7 +31,8 @@ namespace CompactSets
         }
         static Dictionary<string, StatType> nameToStatType = new Dictionary<string, StatType>();
 
-        static readonly Regex statsRegEx = new Regex(@"Adds\s+(?<value>\d+)\s+(?<name>.+)$");
+        //static readonly Regex statsRegEx = new Regex(@"Adds\s+(?<value>\d+)\s+(?<name>\w+\s+\w+)");
+        static readonly Regex statsRegEx = new Regex(@"\s(?<name>(M|H|S|P|W)\w+\s+\w+)");
 
         static void Main(string[] args)
         {
@@ -39,23 +40,37 @@ namespace CompactSets
             nameToStatType["Maximum Magicka"] = new StatType() { Resource = 'M', Factor = 0.3340f };
             nameToStatType["Spell Damage"] = new StatType() { Resource = 'M', Factor = 1 };
             nameToStatType["Spell Critical"] = new StatType() { Resource = 'M', Factor = 1 };
+            nameToStatType["Magic Damage"] = new StatType() { Resource = 'M', Factor = 1 };
+            nameToStatType["Reduce Magicka costs"] = new StatType() { Resource = 'M', Factor = 1 };
+            nameToStatType["Reduce the Magicka cost"] = new StatType() { Resource = 'M', Factor = 1 };
 
             nameToStatType["Health Recovery"] = new StatType() { Resource = 'H', Factor = 1 };
             nameToStatType["Maximum Health"] = new StatType() { Resource = 'H', Factor = 0.3340f };
             nameToStatType["Physical Resistance"] = new StatType() { Resource = 'H', Factor = 1 };
             nameToStatType["Spell Resistance"] = new StatType() { Resource = 'H', Factor = 1 };
+            nameToStatType["Healing Taken"] = new StatType() { Resource = 'H', Factor = 1 };
+            nameToStatType["Reduce damage taken from"] = new StatType() { Resource = 'H', Factor = 1 };
+            nameToStatType["Reduces damage from"] = new StatType() { Resource = 'H', Factor = 1 };
 
             nameToStatType["Stamina Recovery"] = new StatType() { Resource = 'S', Factor = 1 };
             nameToStatType["Maximum Stamina"] = new StatType() { Resource = 'S', Factor = 0.3340f };
             nameToStatType["Weapon Damage"] = new StatType() { Resource = 'S', Factor = 1 };
             nameToStatType["Weapon Critical"] = new StatType() { Resource = 'S', Factor = 1 };
+            nameToStatType["Physical Penetration"] = new StatType() { Resource = 'S', Factor = 1 };
+            nameToStatType["Reduces Stamina cost"] = new StatType() { Resource = 'S', Factor = 1 };
+            nameToStatType["Reduces the costs of Stamina"] = new StatType() { Resource = 'S', Factor = 1 };
+            nameToStatType["Reduce cost of Break Free"] = new StatType() { Resource = 'S', Factor = 1 };
 
-            string filename = @"C:\Users\Votan.Defiant\Data\Documents\Visual Studio 2012\Projects\CompactSets\SetManager_100016.lua";
+            string filename = @"C:\Users\Votan.Defiant\Data\Documents\Visual Studio 2012\Projects\CompactSets\SetManager_100017.lua";
             var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
             var lines = new List<string>(System.IO.File.ReadAllLines(filename));
             lines.RemoveAt(0);
             //for (int i = 0; i < lines.Count; i++) lines[i] = new StringBuilder(lines[i].Trim()).Replace("[\"", "\"").Replace("\"] =", "\":").ToString();
             for (int i = 0; i < lines.Count; i++) lines[i] = Regex.Replace(lines[i].Trim().Replace("] =", "]:"), "\\[(?<num>\\d+)\\]", (MatchEvaluator)delegate(Match match)
+            {
+                return match.Groups["num"].Value;
+            });
+            for (int i = 0; i < lines.Count; i++) lines[i] = Regex.Replace(lines[i], @"\|c\w\w\w\w\w\w(?<num>[^\|]+)\|r", (MatchEvaluator)delegate(Match match)
             {
                 return match.Groups["num"].Value;
             });
@@ -98,52 +113,54 @@ namespace CompactSets
 
                     for (int i = 0; i < bonus.Count; i++)
                     {
-                        var m = statsRegEx.Match(bonus[i]);
-                        if (m.Success)
+                        foreach (Match m in statsRegEx.Matches(bonus[i]))
                         {
-                            var name = m.Groups["name"].Value;
-                            int value;
-                            if (!stats.TryGetValue(name, out value))
-                                value = 0;
-                            // A set which gives a bonus early gets a better vote for that stat
-                            stats[name] = value + Int32.Parse(m.Groups["value"].Value) * 2 / (i + 2) * 4 / bonus.Count;
+                            if (m.Success)
+                            {
+                                var name = m.Groups["name"].Value;
+                                int value;
+                                if (!stats.TryGetValue(name, out value))
+                                    value = 0;
+                                // A set which gives a bonus early gets a better vote for that stat
+                                stats[name] = value + 1;// Int32.Parse(m.Groups["value"].Value) * 2 / (i + 2) * 4 / bonus.Count;
+                            }
                         }
                     }
 
                     var resources = new Dictionary<char, float>();
-                    resources['M'] = 0;
-                    resources['H'] = 0;
-                    resources['S'] = 0;
+                    resources['M'] = 1;
+                    resources['H'] = 1;
+                    resources['S'] = 1;
 
                     foreach (var stat in stats)
                     {
                         StatType statType;
                         if (nameToStatType.TryGetValue(stat.Key, out statType))
-                            resources[statType.Resource] += stat.Value * statType.Factor;
+                            resources[statType.Resource] += stat.Value;// *statType.Factor;
                     }
                     entry.Value["resources"] = resources;
                 }
-                var resourceMax = new Dictionary<char, float>();
-                resourceMax['M'] = 0;
-                resourceMax['H'] = 0;
-                resourceMax['S'] = 0;
+                //var resourceMax = new Dictionary<char, float>();
+                //resourceMax['M'] = 0;
+                //resourceMax['H'] = 0;
+                //resourceMax['S'] = 0;
 
-                foreach (var entry in sets)
-                {
-                    var resources = entry.Value["resources"] as Dictionary<char, float>;
-                    foreach (var resource in resources)
-                    {
-                        resourceMax[resource.Key] = Math.Max(resourceMax[resource.Key], resource.Value);
-                    }
-                }
-                foreach (var entry in sets)
-                {
-                    var resources = entry.Value["resources"] as Dictionary<char, float>;
-                    foreach (var resource in resourceMax)
-                    {
-                        resources[resource.Key] = (float)Math.Round(resources[resource.Key] * 4f / resource.Value + 1.4);
-                    }
-                }
+                //foreach (var entry in sets)
+                //{
+                //    var resources = entry.Value["resources"] as Dictionary<char, float>;
+                //    foreach (var resource in resources)
+                //    {
+                //        resourceMax[resource.Key] = Math.Max(resourceMax[resource.Key], resource.Value);
+                //    }
+                //}
+                //foreach (var entry in sets)
+                //{
+                //    var resources = entry.Value["resources"] as Dictionary<char, float>;
+                //    foreach (var resource in resourceMax)
+                //    {
+                //        resources[resource.Key] = (float)Math.Round(resources[resource.Key] * 4f / resource.Value + 1.4);
+                //    }
+                //}
                 foreach (var entry in sets)
                 {
                     var resources = entry.Value["resources"] as Dictionary<char, float>;
@@ -156,9 +173,9 @@ namespace CompactSets
                     else if (Convert.ToBoolean(entry.Value["isJevelry"]))
                         lua.Append("category = J, ");
 
-                    lua.Append("qualityM = ").Append(resources['M']).Append(", ");
-                    lua.Append("qualityH = ").Append(resources['H']).Append(", ");
-                    lua.Append("qualityS = ").Append(resources['S']).Append(", ");
+                    lua.Append("qualityM = ").Append(Math.Min(5, resources['M'])).Append(", ");
+                    lua.Append("qualityH = ").Append(Math.Min(5, resources['H'])).Append(", ");
+                    lua.Append("qualityS = ").Append(Math.Min(5, resources['S'])).Append(", ");
 
                     lua.Append("items = { ");
                     foreach (var item in (entry.Value["items"] as Dictionary<string, object>).Values)
