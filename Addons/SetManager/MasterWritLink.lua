f6f2915e73054d13ee7bdb52e2dee8ea6db62949
votan <votan@ogitix.de>
2017-11-19 21:30:11 +0100

    SetManager: MasterWritLink: "Show in Popup" context menu.

--- Addons/SetManager/MasterWritLink.lua
+++ Addons/SetManager/MasterWritLink.lua
@@ -27,24 +27,28 @@ do
 		end , "")
 	end
 
-	-- ZO_PreHook("ZO_InventorySlot_DiscoverSlotActionsFromActionList", AddTakeItem)
 	ZO_PreHook("ZO_InventorySlot_RemoveMouseOverKeybinds", RemoveMouseOverKeybinds)
 	ZO_PreHook("ZO_InventorySlot_OnMouseExit", RemoveMouseOverKeybinds)
 	do
-		local orgZO_InventorySlot_DiscoverSlotActionsFromActionList = ZO_InventorySlot_DiscoverSlotActionsFromActionList
-		function ZO_InventorySlot_DiscoverSlotActionsFromActionList(...)
-			if ShowPopupMenu(...) then
-				-- Temporary replace function
-				local orgShow = ZO_InventorySlotActions.Show
-				local inventorySlot, slotActions = ...
-				function ZO_InventorySlotActions.Show(...)
-					AddMenuItem(inventorySlot, slotActions)
-					ZO_InventorySlotActions.Show = orgShow
-					orgShow = nil
-					return ZO_InventorySlotActions.Show(...)
+		local orgZO_InventorySlot_ShowContextMenu = ZO_InventorySlot_ShowContextMenu
+		function ZO_InventorySlot_ShowContextMenu(...)
+			local orgZO_InventorySlot_DiscoverSlotActionsFromActionList = ZO_InventorySlot_DiscoverSlotActionsFromActionList
+			function ZO_InventorySlot_DiscoverSlotActionsFromActionList(...)
+				ZO_InventorySlot_DiscoverSlotActionsFromActionList = orgZO_InventorySlot_DiscoverSlotActionsFromActionList
+				if ShowPopupMenu(...) then
+					-- Temporary replace function
+					local orgShow = ZO_InventorySlotActions.Show
+					local inventorySlot, slotActions = ...
+					function ZO_InventorySlotActions.Show(...)
+						AddMenuItem(inventorySlot, slotActions)
+						ZO_InventorySlotActions.Show = orgShow
+						orgShow = nil
+						return ZO_InventorySlotActions.Show(...)
+					end
 				end
+				return orgZO_InventorySlot_DiscoverSlotActionsFromActionList(...)
 			end
-			return orgZO_InventorySlot_DiscoverSlotActionsFromActionList(...)
+			return orgZO_InventorySlot_ShowContextMenu(...)
 		end
 	end
 end
