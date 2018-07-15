02400eeb245c5423c18ef2048e5ced12bb9df9cd
votan <votan@ogitix.de>
2018-07-15 10:46:07 +0200

    version 1.0.1: - Update to API 100024 "Wolfhunter". - New library load structure.

--- Addons/iChat/LibCustomMenu/LibCustomMenu.lua
+++ Addons/iChat/LibCustomMenu/LibCustomMenu.lua
@@ -2,7 +2,7 @@
 -- thanks to: baertram & circonian
 
 -- Register with LibStub
-local MAJOR, MINOR = "LibCustomMenu", 6.5
+local MAJOR, MINOR = "LibCustomMenu", 6.6
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end -- the same or newer version of this lib is already loaded into memory
 
