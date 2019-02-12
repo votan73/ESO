6dd428c89dca1b026c9b8a271c89003722c92e8a
Marc <scootworks@me.com>
2019-02-12 10:08:01 +0100

    healthWarner
    added healthWarner basics

---
+++ Addons/UnitFramesRebirth/HealthWarner.lua
@@ -0,0 +1,73 @@
+local HEALTH_ALPHA_PULSE_THRESHOLD = 0.25
+local HEALTH_WARNER_FLASH_TIME  = 300
+
+UnitFramesRebirth_HealthWarner = ZO_Object:Subclass()
+
+function UnitFramesRebirth_HealthWarner:New(...)
+	local warner = ZO_Object.New(self)
+	warner:Initialize(...)
+	return warner
+end
+
+function UnitFramesRebirth_HealthWarner:Initialize(parent, unitTag, style)
+
+	df("HealthWarner - style: %s", style)
+	
+	if not style == "ZO_GroupUnitFrame" then
+		return
+	end
+	
+	local function OnPowerUpdate(_, unitTag, powerIndex, powerType, health, maxHealth)
+		self:OnHealthUpdate(health, maxHealth)
+	end
+	local function OnPlayerActivated()
+		local current, max = GetUnitPower(unitTag, POWERTYPE_HEALTH)
+		self:OnHealthUpdate(current, max)
+	end
+
+	self.warning = GetControl(parent, "Warner")
+	self.unitTag = unitTag
+
+	self.warnAnimation = ZO_AlphaAnimation:New(self.warning)
+	self.statusBar = parent
+	self.paused = false
+
+	self.warning:RegisterForEvent(EVENT_POWER_UPDATE, OnPowerUpdate)
+	self.warning:AddFilterForEvent(EVENT_POWER_UPDATE, REGISTER_FILTER_POWER_TYPE, POWERTYPE_HEALTH, REGISTER_FILTER_UNIT_TAG, unitTag)
+	self.warning:RegisterForEvent(EVENT_PLAYER_ACTIVATED, OnPlayerActivated)
+end
+
+function UnitFramesRebirth_HealthWarner:SetPaused(paused)
+	if self.paused ~= paused then
+		self.paused = paused
+		if paused then
+			if self.warnAnimation:IsPlaying() then
+				self.warnAnimation:Stop()
+			end
+		else
+			local current, max = GetUnitPower(unitTag, POWERTYPE_HEALTH)
+			self.warning:SetAlpha(0)
+			self:UpdateAlphaPulse(current / max)
+		end
+	end
+end
+
+function UnitFramesRebirth_HealthWarner:UpdateAlphaPulse(healthPerc)
+	if healthPerc <= HEALTH_ALPHA_PULSE_THRESHOLD then
+		if not self.warnAnimation:IsPlaying() then
+			self.warnAnimation:PingPong(0, 1, HEALTH_WARNER_FLASH_TIME)
+		end
+	else
+		if self.warnAnimation:IsPlaying() then
+			self.warnAnimation:Stop()
+			self.warning:SetAlpha(0)
+		end
+	end
+end
+
+function UnitFramesRebirth_HealthWarner:OnHealthUpdate(health, maxHealth)
+	if not self.paused then
+		local healthPerc = health / maxHealth
+		self:UpdateAlphaPulse(healthPerc)
+	end
+end
