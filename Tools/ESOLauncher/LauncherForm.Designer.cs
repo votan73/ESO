a65eeb2d85a806d2b30491b536d8bbac78b3fc67
votan <votan@ogitix.de>
2022-11-27 12:00:39 +0100

    ESO Launcher
    Start ESO with high prio.

---
+++ Tools/ESOLauncher/LauncherForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ESOLauncher
+{
+    partial class LauncherForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartNA = new System.Windows.Forms.Button();
+            this.btnStartEU = new System.Windows.Forms.Button();
+            this.btnStartPTS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStartNA
+            // 
+            this.btnStartNA.Location = new System.Drawing.Point(12, 12);
+            this.btnStartNA.Name = "btnStartNA";
+            this.btnStartNA.Size = new System.Drawing.Size(128, 128);
+            this.btnStartNA.TabIndex = 0;
+            this.btnStartNA.Text = "NA";
+            this.btnStartNA.UseVisualStyleBackColor = true;
+            this.btnStartNA.Click += new System.EventHandler(this.StartNA_Click);
+            // 
+            // btnStartEU
+            // 
+            this.btnStartEU.Location = new System.Drawing.Point(146, 12);
+            this.btnStartEU.Name = "btnStartEU";
+            this.btnStartEU.Size = new System.Drawing.Size(128, 128);
+            this.btnStartEU.TabIndex = 1;
+            this.btnStartEU.Text = "EU";
+            this.btnStartEU.UseVisualStyleBackColor = true;
+            this.btnStartEU.Click += new System.EventHandler(this.StartEU_Click);
+            // 
+            // btnStartPTS
+            // 
+            this.btnStartPTS.Location = new System.Drawing.Point(281, 12);
+            this.btnStartPTS.Name = "btnStartPTS";
+            this.btnStartPTS.Size = new System.Drawing.Size(128, 128);
+            this.btnStartPTS.TabIndex = 2;
+            this.btnStartPTS.Text = "PTS";
+            this.btnStartPTS.UseVisualStyleBackColor = true;
+            this.btnStartPTS.Click += new System.EventHandler(this.StartPTS_Click);
+            // 
+            // LauncherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(421, 150);
+            this.Controls.Add(this.btnStartPTS);
+            this.Controls.Add(this.btnStartEU);
+            this.Controls.Add(this.btnStartNA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "LauncherForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ESO Launcher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartNA;
+        private System.Windows.Forms.Button btnStartEU;
+        private System.Windows.Forms.Button btnStartPTS;
+    }
+}
+
---
+++ Tools/ESOLauncher/LauncherForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESOLauncher
+{
+    public partial class LauncherForm : Form
+    {
+        public LauncherForm()
+        {
+            InitializeComponent();
+        }
+        static Font font;
+        System.IO.FileInfo fileLive;
+        System.IO.FileInfo filePTS;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            fileLive = new System.IO.FileInfo(@"..\The Elder Scrolls Online\game\client\eso64.exe");
+            filePTS = new System.IO.FileInfo(@"..\The Elder Scrolls Online PTS\game\client\eso64.exe");
+
+            font = font ?? new Font(Font.FontFamily, 36);
+            btnStartPTS.Font = btnStartEU.Font = btnStartNA.Font = font;
+
+
+            btnStartNA.Enabled = btnStartEU.Enabled = fileLive.Exists;
+            btnStartPTS.Enabled = filePTS.Exists;
+
+            Icon = Program.Icon;
+            //try
+            //{
+            //    Icon = new Icon("ESO.ico");
+            //}
+            //catch (System.IO.FileNotFoundException) { }
+            base.OnLoad(e);
+        }
+        private static void UpdateSettings(bool isPTS, bool isEU)
+        {
+            if (isPTS)
+                return;
+
+            var settingsFile = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Elder Scrolls Online", "live", "UserSettings.txt");
+            if (!System.IO.File.Exists(settingsFile))
+                return;
+            var lines = new List<string>(System.IO.File.ReadAllLines(settingsFile));
+            var plattform = "SET LastPlatform " + (isEU ? "\"Live-EU\"" : "\"Live\"");
+            var realm = "SET LastRealm " + (isEU ? "\"EU Megaserver\"" : "\"NA Megaserver\"");
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                if (line.StartsWith("SET LastRealm ", StringComparison.OrdinalIgnoreCase))
+                {
+                    lines[i] = realm;
+                }
+                else if (line.StartsWith("SET LastPlatform ", StringComparison.OrdinalIgnoreCase))
+                {
+                    lines[i] = plattform;
+                }
+            }
+            System.IO.File.WriteAllLines(settingsFile, lines.ToArray());
+        }
+
+        private void Launch(object sender, System.IO.FileInfo file)
+        {
+            var btn = sender as Button;
+            btn.Enabled = false;
+            Task.Factory.StartNew(() =>
+            {
+                var info = new System.Diagnostics.ProcessStartInfo(file.FullName);
+                info.WorkingDirectory = file.Directory.FullName;
+                info.UseShellExecute = false;
+                var p = System.Diagnostics.Process.Start(info);
+                p.PriorityBoostEnabled = true;
+                p.PriorityClass = System.Diagnostics.ProcessPriorityClass.High;
+                p.WaitForExit();
+                if (IsHandleCreated)
+                {
+                    MethodInvoker exited = () => btn.Enabled = file.Exists;
+                    BeginInvoke(exited);
+                }
+            });
+        }
+
+        private void StartNA_Click(object sender, EventArgs e)
+        {
+            UpdateSettings(false, false);
+            Launch(sender, fileLive);
+        }
+
+        private void StartEU_Click(object sender, EventArgs e)
+        {
+            UpdateSettings(false, true);
+            Launch(sender, fileLive);
+        }
+        private void StartPTS_Click(object sender, EventArgs e)
+        {
+            UpdateSettings(true, false);
+            Launch(sender, filePTS);
+        }
+
+    }
+}
