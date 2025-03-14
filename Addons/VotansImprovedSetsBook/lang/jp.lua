106edc706d2f247bc3b61cd46d0f129f3c3dd9cb
Warashi_JP <122269877+Warashi-JP@users.noreply.github.com>
2025-03-14 18:05:05 +0900

    Add "jp.lua"
    I did the Japanese translation.
    I'm sorry to bother you when you're busy.
    We would appreciate it if you could check it when you have time.

---
+++ Addons/VotansImprovedSetsBook/lang/jp.lua
@@ -0,0 +1,9 @@
+-- Translation by @snorunt361(ESOUI),@Warashi-JP(github)
+
+ZO_CreateStringId("SI_VOTANS_IMPROVED_SETS_BOOK_FAVORITES", "お気に入り")
+ZO_CreateStringId("SI_VOTANS_IMPROVED_SETS_BOOK_FAVORITE_ADD", "お気に入りに追加")
+ZO_CreateStringId("SI_VOTANS_IMPROVED_SETS_BOOK_FAVORITE_REMOVE", "お気に入りから削除")
+ZO_CreateStringId("SI_VOTANS_SETSBOOK_LAYER", "VotansSetsBook")
+ZO_CreateStringId("SI_VOTANS_IMPROVED_SETS_BOOK_LOCATION", "現在地")
+
+ZO_CreateStringId("SI_VOTANS_IMPROVED_SETS_BOOK_FAVORITE_POPUP", "ポップアップで表示")
