42af551ccd32971dc4b47c90da9923d1a8269e4e
votan <votan@ogitix.de>
2025-06-14 13:39:31 +0200

    version 1.5.0: - manifest update. - aliases are account-wide.

--- Addons/HarvensAliases/HarvensAliases.lua
+++ Addons/HarvensAliases/HarvensAliases.lua
@@ -6,31 +6,32 @@ function HarvensAliases_DoCommandInClosure(alias, cmd)
 		args = cmd[2]
 		cmd = cmd[1]
 	end
-		
-	SLASH_COMMANDS["/"..alias] = function(userArgs)
-		if SLASH_COMMANDS["/"..cmd] ~= nil and type(SLASH_COMMANDS["/"..cmd]) == "function" then
+
+	local slashCommand = "/" .. cmd
+	SLASH_COMMANDS["/" .. alias] = function(userArgs)
+		if SLASH_COMMANDS[slashCommand] ~= nil and type(SLASH_COMMANDS[slashCommand]) == "function" then
 			if args then
 				if #userArgs > 0 then
-					SLASH_COMMANDS["/"..cmd](args.." "..userArgs)
+					SLASH_COMMANDS[slashCommand](args .. " " .. userArgs)
 				else
-					SLASH_COMMANDS["/"..cmd](args)
+					SLASH_COMMANDS[slashCommand](args)
 				end
 			else
-				SLASH_COMMANDS["/"..cmd](userArgs)
+				SLASH_COMMANDS[slashCommand](userArgs)
 			end
 		else
-			CHAT_SYSTEM:AddMessage("Command /"..cmd.." doesn't exists")
+			CHAT_ROUTER:AddSystemMessage("Command /" .. cmd .. " doesn't exists")
 		end
 	end
 end
 
 function HarvensAliases_ListAliases(...)
-	CHAT_SYSTEM:AddMessage("Defined aliases:")
-	for k,v in pairs(sv.aliases) do
+	CHAT_ROUTER:AddSystemMessage("Defined aliases:")
+	for k, v in pairs(sv.aliases) do
 		if type(v) == "table" then
-			CHAT_SYSTEM:AddMessage(k.." -> "..v[1].."("..table.concat(v, ", ", 2)..")")
+			CHAT_ROUTER:AddSystemMessage(string.format("%s -> %s(%s)", k, v[1], table.concat(v, ", ", 2)))
 		else
-			CHAT_SYSTEM:AddMessage(k.." -> "..v)
+			CHAT_ROUTER:AddSystemMessage(string.format("%s -> %s", k, v))
 		end
 	end
 end
@@ -38,70 +39,77 @@ end
 function HarvensAliases_AddAlias(arguments)
 	local args = {}
 	local i = 1
-	for w in string.gmatch(arguments,"[%w_%-]+") do
+	for w in string.gmatch(arguments, "[%w_%-]+") do
 		args[i] = w
 		i = i + 1
 	end
-	
+
 	if args[1] == nil or args[2] == nil then
-		CHAT_SYSTEM:AddMessage("Usage: /addalias <alias> <command> <default arguments>")
-		CHAT_SYSTEM:AddMessage("Example: /addalias rl reloadui")
+		CHAT_ROUTER:AddSystemMessage("Usage: /addalias <alias> <command> <default arguments>")
+		CHAT_ROUTER:AddSystemMessage("Example: /addalias rl reloadui")
 		return
 	end
-	
+
 	local alias = args[1]
 	local cmd = args[2]
-	if SLASH_COMMANDS["/"..alias] ~= nil then
-		CHAT_SYSTEM:AddMessage("Command /"..alias.." already exists.")
+	if SLASH_COMMANDS["/" .. alias] ~= nil then
+		CHAT_ROUTER:AddSystemMessage("Command /" .. alias .. " already exists.")
 		return
 	end
-	
+
 	if cmd == alias then
-		CHAT_SYSTEM:AddMessage("Alias name and command cannot be the same.")
+		CHAT_ROUTER:AddSystemMessage("Alias name and command cannot be the same.")
 		return
 	end
-	
+
 	local aliasArgs = table.concat(args, " ", 3)
-	
+
 	sv.aliases[alias] = {cmd, aliasArgs}
 	HarvensAliases_DoCommandInClosure(alias, sv.aliases[alias])
-	CHAT_SYSTEM:AddMessage("Alias /"..alias.." added.")
+	CHAT_ROUTER:AddSystemMessage("Alias /" .. alias .. " added.")
 end
 
 function HarvensAliases_DelAlias(arguments)
 	local args = {}
 	local i = 1
-	for w in string.gmatch(arguments,"[%w_%-]+") do
+	for w in string.gmatch(arguments, "[%w_%-]+") do
 		args[i] = w
 		i = i + 1
 	end
-	
+
 	if args[1] == nil then
-		CHAT_SYSTEM:AddMessage("Usage: /delalias <alias>")
-		CHAT_SYSTEM:AddMessage("Example: /delalias rl")
+		CHAT_ROUTER:AddSystemMessage("Usage: /delalias <alias>")
+		CHAT_ROUTER:AddSystemMessage("Example: /delalias rl")
 		return
 	end
-	
+
 	if sv.aliases[args[1]] == nil then
-		CHAT_SYSTEM:AddMessage("Alias /"..args[1].." does not exists.")
+		CHAT_ROUTER:AddSystemMessage("Alias /" .. args[1] .. " does not exists.")
 		return
 	end
-	
-	SLASH_COMMANDS["/"..args[1]] = nil
+
+	SLASH_COMMANDS["/" .. args[1]] = nil
 	sv.aliases[args[1]] = nil
-	CHAT_SYSTEM:AddMessage("Alias /"..args[1].." deleted.")
+	CHAT_ROUTER:AddSystemMessage("Alias /" .. args[1] .. " deleted.")
 end
 
 function HarvensAliasesInitialize(eventCode, name)
-	if name ~= "HarvensAliases" then return end
-	
-	local defaults = { aliases = {}}
-	sv = ZO_SavedVars:New("HarvensAliases_SavedVariables", 1, nil, defaults)
-	
-	for k,v in pairs(sv.aliases) do
-		HarvensAliases_DoCommandInClosure(k,v)
+	if name ~= "HarvensAliases" then
+		return
 	end
-	
+
+	local defaults = {aliases = {}}
+	sv = ZO_SavedVars:New("HarvensAliases_SavedVariables", 1, nil, nil)
+	if sv.aliases then
+		defaults.aliases = sv.aliases
+		sv.aliases = nil
+	end
+	sv = ZO_SavedVars:NewAccountWide("HarvensAliases_SavedVariables", 1, nil, defaults)
+
+	for k, v in pairs(sv.aliases) do
+		HarvensAliases_DoCommandInClosure(k, v)
+	end
+
 	SLASH_COMMANDS["/addalias"] = HarvensAliases_AddAlias
 	SLASH_COMMANDS["/listaliases"] = HarvensAliases_ListAliases
 	SLASH_COMMANDS["/delalias"] = HarvensAliases_DelAlias
