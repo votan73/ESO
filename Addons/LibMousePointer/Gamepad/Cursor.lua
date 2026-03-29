893d57bd4943491a1a249043e27749a2e4c75497
votan <votan@ogitix.de>
2026-03-29 17:12:30 +0200

    version 1.0.0: - "Mouse" pointer for gamepad and console.

---
+++ Addons/LibMousePointer/Gamepad/Cursor.lua
@@ -0,0 +1,111 @@
+local lib = LibMousePointer
+local Cursor_Gamepad = ZO_InitializingObject:Subclass()
+lib.Cursor_Gamepad = Cursor_Gamepad
+local CURSOR_SPEED = 5
+
+function Cursor_Gamepad:Initialize(control)
+    self.control = control
+    self.control:SetHidden(true)
+    self.initialX, self.initialY = GuiRoot:GetCenter()
+    self:ResetPosition()
+end
+
+function Cursor_Gamepad:UpdateDirectionalInput()
+    if not DIRECTIONAL_INPUT:IsAvailable(ZO_DI_LEFT_STICK) then
+        --d("Directional input not available, cannot update cursor position.")
+        return
+    end
+
+    local dx, dy = DIRECTIONAL_INPUT:GetXY(ZO_DI_LEFT_STICK)
+    dx, dy = zo_clampLength2D(dx, dy, 1.0) -- clamp dpad output
+    local frameDelta = GetFrameDeltaNormalizedForTargetFramerate()
+    local magnitude = frameDelta * CURSOR_SPEED
+    dx = dx * magnitude
+    dy = -dy * magnitude
+
+    self.control:SetAnchor(CENTER, GuiRoot, TOPLEFT, self.x + dx, self.y + dy)
+    local clampedX, clampedY = self.control:GetCenter()
+    if clampedX ~= self.x or clampedY ~= self.y then
+        self.x, self.y = clampedX, clampedY
+        self.directionalX = self.x + dx
+        self.directionalY = self.y + dy
+    end
+
+    self:UpdateCursorInfo()
+end
+
+function Cursor_Gamepad:UpdateVirtualMousePosition()
+    if self.cursorId ~= nil then
+        local deltaX, deltaY = GetUIMouseDeltas()
+        if deltaX ~= 0 or deltaY ~= 0 then
+            -- This code allows the player to use the mouse on the gamepad UI on a PC build.
+            -- If movement is coming from directional input (ei. arrows, joystick) then the
+            -- mouse should jump to the crosshairs when used again. When mouse is the primary
+            -- mode of movement then the gamepad crosshairs (self.control) will follow.
+            local mouseX, mouseY = self.directionalX, self.directionalY
+            if mouseX == nil or mouseY == nil then
+                mouseX, mouseY = GetUIMousePosition()
+            else
+                WINDOW_MANAGER:SetMouseFocusByName(self.control:GetName())
+                self.directionalX = nil
+                self.directionalY = nil
+            end
+
+            self.control:SetAnchor(CENTER, GuiRoot, TOPLEFT, mouseX, mouseY)
+            local clampedX, clampedY = self.control:GetCenter()
+            if clampedX ~= self.x or clampedY ~= self.y then
+                self.x, self.y = clampedX, clampedY
+                return true
+            end
+        end
+    end
+    return false
+end
+
+function Cursor_Gamepad:OnUpdate()
+    if self:UpdateVirtualMousePosition() then
+        self:UpdateCursorInfo()
+    end
+end
+
+function Cursor_Gamepad:UpdateCursorInfo()
+    local counterScrollX = (self.x - self.initialX) * ZO_CHAMPION_COUNTERSCROLL_FACTOR_X
+    local counterScrollY = (self.y - self.initialY) * -ZO_CHAMPION_COUNTERSCROLL_FACTOR_Y
+
+    WINDOW_MANAGER:UpdateCursorPosition(self.cursorId, self.x, self.y)
+    local mouseOverControl = WINDOW_MANAGER:GetControlAtCursor(self.cursorId)
+    self.mouseOverControl = mouseOverControl
+end
+
+function Cursor_Gamepad:UpdateVisibility(show)
+    self.control:SetHidden(not show)
+
+    if show then
+        if not self.cursorId then
+            self.cursorId = WINDOW_MANAGER:CreateCursor(self.x, self.y)
+        end
+    else
+        if self.cursorId then
+            WINDOW_MANAGER:DestroyCursor(self.cursorId)
+            self.cursorId = nil
+        end
+    end
+
+    if not show then
+        self.mouseOverControl = nil
+    end
+end
+
+function Cursor_Gamepad:ResetPosition()
+    self.x, self.y = self.initialX, self.initialY
+    self.control:SetAnchor(CENTER, GuiRoot, TOPLEFT, self.x, self.y)
+    self:UpdateCursorInfo()
+end
+
+function Cursor_Gamepad:GetCursorPosition()
+    return WINDOW_MANAGER:GetCursorPosition(self.cursorId)
+end
+
+function Cursor_Gamepad:GetControlAtCursor(desiredHandlers)
+    return desiredHandlers and WINDOW_MANAGER:GetControlAtCursor(self.cursorId, desiredHandlers) or self.mouseOverControl
+end
---
+++ Addons/LibMousePointer/Gamepad/Main.lua
@@ -0,0 +1,110 @@
+local lib = {}
+
+lib.name = "LibMousePointer"
+
+function lib:Initialize()
+    self.control = LibMousePointerTopLevel
+    local fragment = ZO_FadeSceneFragment:New(self.control)
+
+    lib.keybindButton = {
+        alignment = KEYBIND_STRIP_ALIGN_LEFT,
+        order = -1000,
+        name = GetString(SI_GAMEPAD_SELECT_OPTION),
+        keybind = "UI_SHORTCUT_PRIMARY",
+        callback = function(keyUp)
+            self:InvokeClick()
+        end
+    }
+
+    local buttonApplied = false
+    fragment:RegisterCallback(
+        "StateChange",
+        function(oldState, newState)
+            if newState == SCENE_FRAGMENT_SHOWING then
+                if not self.gamepadCursor then
+                    self.gamepadCursor = self.Cursor_Gamepad:New(self.control:GetNamedChild("GamepadCursor"))
+                end
+                self.control:BringWindowToTop()
+            elseif newState == SCENE_FRAGMENT_SHOWN then
+                self.gamepadCursor:UpdateVisibility(true)
+                DIRECTIONAL_INPUT:Activate(self.gamepadCursor, self.control)
+                if not KEYBIND_STRIP:HasKeybindButton(self.keybindButton) then
+                    KEYBIND_STRIP:AddKeybindButton(self.keybindButton)
+                    buttonApplied = true
+                end
+            elseif newState == SCENE_FRAGMENT_HIDING then
+                if buttonApplied then
+                    KEYBIND_STRIP:RemoveKeybindButton(self.keybindButton)
+                    buttonApplied = false
+                end
+                DIRECTIONAL_INPUT:Deactivate(self)
+                self.gamepadCursor:UpdateVisibility(false)
+            end
+        end
+    )
+
+    MOUSE_POINTER_FRAGMENT = fragment
+end
+
+function lib:UpdateDirectionalInput()
+    self.gamepadCursor:UpdateDirectionalInput()
+end
+
+function lib:ResetPosition()
+    self.gamepadCursor:ResetPosition()
+end
+
+function lib:GetCursorPosition()
+    if not self.gamepadCursor then
+        return -1, -1
+    end
+    return self.gamepadCursor:GetCursorPosition()
+end
+
+function lib:GetControlAtCursor(desiredHandlers)
+    if not self.gamepadCursor then
+        return nil
+    end
+    return self.gamepadCursor:GetControlAtCursor(desiredHandlers)
+end
+
+function lib:InvokeClick(button, desiredHandlers)
+    if not self.gamepadCursor then
+        return
+    end
+    local control = self:GetControlAtCursor(desiredHandlers)
+    if not control then
+        return
+    end
+    button = button or MOUSE_BUTTON_INDEX_LEFT
+    local handler = control:GetHandler("OnClicked")
+    if handler then
+        handler(control, button)
+        return
+    end
+    handler = control:GetHandler("OnMouseDown")
+    if handler then
+        handler(control, button)
+    end
+    handler = control:GetHandler("OnMouseUp")
+    if handler then
+        zo_callLater(
+            function()
+                handler(control, button)
+            end,
+            100
+        )
+    end
+end
+
+local function AddonLoaded(eventType, addonName)
+    if addonName ~= lib.name then
+        return
+    end
+    EVENT_MANAGER:UnregisterForEvent(lib.name, EVENT_ADD_ON_LOADED)
+    lib:Initialize()
+end
+
+EVENT_MANAGER:RegisterForEvent(lib.name, EVENT_ADD_ON_LOADED, AddonLoaded)
+
+LibMousePointer = lib
